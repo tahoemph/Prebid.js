075e5924b1a250a3d3bfc806d942a4b5cc482ecc
Michael Hunter <mhunter@saymedia.com>
2015-10-08 21:16:56 -0700

    rebuild

--- dist/prebid.js
+++ dist/prebid.js
@@ -1,5 +1,5 @@
 /* Prebid.js v0.3.2 
-Updated : 2015-10-08 */
+Updated : 2015-10-09 */
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /** @module adaptermanger */
 
@@ -162,7 +162,7 @@ var AppNexusAdapter = function AppNexusAdapter() {
 		jptCall = utils.tryAppendQueryString(jptCall, 'callback_uid', callbackId);
 
 		//disable PSAs here, as per RAD-503
-		jptCall = utils.tryAppendQueryString(jptCall, 'psa', '0');
+		// jptCall = utils.tryAppendQueryString(jptCall, 'psa', '0');
 		jptCall = utils.tryAppendQueryString(jptCall, 'id', placementId);
 		jptCall = utils.tryAppendQueryString(jptCall, 'member_id', memberId);
 		jptCall = utils.tryAppendQueryString(jptCall, 'code', inventoryCode);
@@ -182,7 +182,7 @@ var AppNexusAdapter = function AppNexusAdapter() {
 		}
 
 		//append referrer
-		jptCall = utils.tryAppendQueryString(jptCall, 'referrer', utils.getTopWindowUrl());
+		// jptCall = utils.tryAppendQueryString(jptCall, 'referrer', utils.getTopWindowUrl());
 
 
 		//remove the trailing "&"
